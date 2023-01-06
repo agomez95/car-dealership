a02b05f11c5bcd0ac2e38b52a3d525aaa5c109e5
Adrian Gomez V <sandmancity14@gmail.com>
2023-01-06 18:41:00 -0500

    se creo el brand con totalidad y se empezo con el seed

--- src/brands/brands.service.ts
+++ src/brands/brands.service.ts
@@ -1,26 +1,65 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBrandDto } from './dto/create-brand.dto';
 import { UpdateBrandDto } from './dto/update-brand.dto';
+import {v4 as uuid} from 'uuid'
+import { Brand } from './entities/brand.entity';
+import { BadRequestException, NotFoundException } from '@nestjs/common/exceptions';
 
 @Injectable()
 export class BrandsService {
+
+  private brands: Brand[] = [
+    {
+      id: uuid(),
+      name: 'Toyota',
+      createdAt: new Date().getTime()
+    }
+  ]
+
   create(createBrandDto: CreateBrandDto) {
-    return 'This action adds a new brand';
+
+    const {name} = createBrandDto;
+
+    const brand: Brand = {
+      id: uuid(),
+      name: name.toLocaleLowerCase(),
+      createdAt: new Date().getTime()
+    }
+
+    this.brands.push(brand);
+
+    return brand;
   }
 
   findAll() {
-    return `This action returns all brands`;
+    return this.brands;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} brand`;
+  findOne(id: string) {
+    const brand = this.brands.find(brand => brand.id === id);
+
+    if(!brand) throw new NotFoundException(`Brand with id '${id}' not found`);
+
+    return brand;
   }
 
-  update(id: number, updateBrandDto: UpdateBrandDto) {
-    return `This action updates a #${id} brand`;
+  update(id: string, updateBrandDto: UpdateBrandDto) {
+    let brandFind = this.findOne(id);
+
+    this.brands = this.brands.map(brand => {
+      if(brand.id === id) {
+        brandFind.updatedAt = new Date().getTime()
+        brandFind = {...brandFind, ...updateBrandDto, id}
+        return brandFind;
+      }
+      return brand;
+    });
+
+    return brandFind;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} brand`;
+  remove(id: string) {
+    this.brands = this.brands.filter(brand => brand.id !== id);
+    return `The car with id ${id} was deleted`;
   }
 }
